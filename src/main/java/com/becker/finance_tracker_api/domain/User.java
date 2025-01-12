27396cd19099f168089be8ee1b0cc9af3e17e430
camila becker <camila.becker01@gmail.com>
2025-01-12 11:51:24 -0300

    Criado entidade User

---
+++ src/main/java/com/becker/finance_tracker_api/domain/User.java
@@ -0,0 +1,33 @@
+package com.becker.finance_tracker_api.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
